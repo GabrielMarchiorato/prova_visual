1d662545f3a784667f5f4d6532e26ce511067329
vihernandes <vinicius.hernandes@indt.org.br>
2021-11-25 18:51:29 -0300

    other changes

--- API/Models/FormaPagamento.cs
+++ API/Models/FormaPagamento.cs
@@ -6,7 +6,7 @@ namespace API.Models
     public class FormaPagamento
     {
         public FormaPagamento() => CriadoEm = DateTime.Now;
-        public int CategoriaId { get; set; }
+        public int FormaPagamentoId { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public DateTime CriadoEm { get; set; }
---
+++ FRONT/src/app/components/views/forma-pagamento/listar-formas/listar-forma-pagamento.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from "@angular/core";
+import { FormaPagamento } from "src/app/models/forma-pagamento";
+import { Produto } from "src/app/models/produto";
+import { FormaPagamentoService } from "src/app/services/forma-pagamento.service";
+import { ProdutoService } from "src/app/services/produto.service";
+
+@Component({
+    selector: "app-listar-forma-pagamento",
+    templateUrl: "./listar-forma-pagamento.component.html",
+    styleUrls: ["./listar-forma-pagamento.component.css"],
+})
+export class ListarFormaPagamentoComponent implements OnInit {
+    formas: FormaPagamento[] = [];
+    colunasExibidas: String[] = [
+        "id",
+        "nome",
+        "descricao",
+        "preco",
+        "quantidade",
+        "categoria",
+    ];
+
+    constructor(private service: FormaPagamentoService) {}
+
+    ngOnInit(): void {
+        this.service.list().subscribe((formas) => {
+            this.formas = formas;
+        });
+    }
+}
